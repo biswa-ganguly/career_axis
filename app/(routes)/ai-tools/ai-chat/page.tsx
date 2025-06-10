224f595b60b309b005b2efee14c1194726945da9
biswa-ganguly <biswaganguly16@gmail.com>
2025-06-10 22:34:04 +0530

    `Added header, button, and empty state sections to AI Chat page`

--- app/(routes)/ai-tools/ai-chat/page.tsx
+++ app/(routes)/ai-tools/ai-chat/page.tsx
@@ -1,8 +1,30 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 function AiChat() {
   return (
-    <div>AiChat</div>
+    <div>
+      <div className="flex items-center justify-between  mb-4">
+        <div className="">
+        <h1 className='text-2xl font-semibold mb-2'>AI Chat Question & Answer</h1>
+        <p className='text-gray-500'>Smarter career decisions start here — get tailored advice, real-time market insights, and a roadmap built just for you with the power of AI.</p>
+      </div>
+      <Button>+New Chat</Button>
+      </div>
+
+      <div>
+        {/* Empty state Options */}
+      </div>
+
+      <div>
+        {/* Message List */}
+      </div>
+
+      <div>
+        {/* Message Input */}
+      </div>
+
+    </div>
   )
 }
 
